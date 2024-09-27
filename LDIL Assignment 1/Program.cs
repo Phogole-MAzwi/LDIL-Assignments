1b8388162dda8b35bccdd9fd3b7d281c05251964
Phogole-MAzwi <phogoleam@gmail.com>
2024-09-27 08:49:58 +0200

    Add project files.

---
+++ LDIL Assignment 1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace LDIL_Assignment_1
+{
+    public class BeerSong
+    {
+        public void SingSong()
+        {
+            int bottles = 99;
+
+            do
+            {
+                if (bottles > 2)
+                {
+                    Console.WriteLine($"{bottles} bottles of beer on the wall, {bottles} bottles of beer.");
+                    Console.WriteLine($"Take one down and pass it around, {bottles - 1} bottles of beer on the wall.");
+                }
+                else
+                {
+                    Console.WriteLine("2 bottle of beer on the wall, 2 bottle of beer.");
+                    Console.WriteLine("Take one down and pass it around, no more bottles of beer on the wall.");
+
+                }
+
+                Console.WriteLine();
+                bottles--;
+
+            } while (bottles > 1);
+
+            Console.WriteLine("1 bottle of beer on the wall, 1 bottle of beer.");
+            Console.WriteLine("Take one down and pass it around, no more bottles of beer on the wall.");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            BeerSong song = new BeerSong();
+            song.SingSong();
+        }
+    }
+}
